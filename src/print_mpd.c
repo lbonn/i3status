09f80978679da2719780650c56bb32cd7dc5e820
lbonn <bonnans.l@gmail.com>
2013-12-09 18:27:34 +0100

    mpd: better style

--- src/print_mpd.c
+++ src/print_mpd.c
@@ -33,6 +33,7 @@ void print_mpd(yajl_gen json_gen, char *buffer, const char *host, int port, cons
 
         static struct mpd_connection *conn;
         struct mpd_status *status = NULL;
+        enum mpd_state state;
         struct mpd_song *song;
 
         /* First run */
@@ -62,65 +63,65 @@ void print_mpd(yajl_gen json_gen, char *buffer, const char *host, int port, cons
                 }
         }
 
-        status = mpd_run_status(conn);
+        if ((status = mpd_run_status(conn))) {
+                state = mpd_status_get_state(status);
+        }
 
-        if ((status != NULL) &&
-                        (mpd_status_get_state(status) == MPD_STATE_PLAY ||
-                         mpd_status_get_state(status) == MPD_STATE_PAUSE)) {
-
-                if (mpd_status_get_state(status) == MPD_STATE_PLAY)
-                        START_COLOR("color_good");
-                else if (mpd_status_get_state(status) == MPD_STATE_PAUSE)
-                        START_COLOR("color_degraded");
-
-                mpd_status_free(status);
-                song = mpd_run_current_song(conn);
-
-                COPY_CROP(titlebuf, mpd_song_get_tag(song, MPD_TAG_TITLE, 0));
-                COPY_CROP(artistbuf, mpd_song_get_tag(song, MPD_TAG_ARTIST, 0));
-                COPY_CROP(albumbuf, mpd_song_get_tag(song, MPD_TAG_ALBUM, 0));
-                COPY_CROP(trackbuf, mpd_song_get_tag(song, MPD_TAG_TRACK, 0));
-                COPY_CROP(datebuf, mpd_song_get_tag(song, MPD_TAG_DATE, 0));
-
-                mpd_song_free(song);
-
-                for (walk = format; *walk != '\0'; walk++) {
-                        if (*walk != '%') {
-                                *(outwalk++) = *walk;
-                                continue;
-                        }
-
-                        if (BEGINS_WITH(walk+1, "title")) {
-                                if (*titlebuf)
-                                        outwalk += sprintf(outwalk, "%s", titlebuf);
-                                walk += strlen("title");
-                        }
-                        else if (BEGINS_WITH(walk+1, "artist")) {
-                                if (*artistbuf)
-                                        outwalk += sprintf(outwalk, "%s", artistbuf);
-                                walk += strlen("artist");
-                        }
-                        else if (BEGINS_WITH(walk+1, "album")) {
-                                if (*albumbuf)
-                                        outwalk += sprintf(outwalk, "%s", albumbuf);
-                                walk += strlen("album");
-                        }
-                        else if (BEGINS_WITH(walk+1, "track")) {
-                                if (*trackbuf)
-                                        outwalk += sprintf(outwalk, "%s", trackbuf);
-                                walk += strlen("track");
-                        }
-                        else if (BEGINS_WITH(walk+1, "date")) {
-                                if (*datebuf)
-                                        outwalk += sprintf(outwalk, "%s", datebuf);
-                                walk += strlen("date");
-                        }
-                }
-        } else {
+        if (!status || state != MPD_STATE_PLAY || state != MPD_STATE_PAUSE) {
                 START_COLOR("color_bad");
                 outwalk += sprintf(outwalk, "%s", "Stopped");
                 mpd_connection_free(conn);
                 conn = NULL;
+                goto print_end;
+        }
+
+        if (state == MPD_STATE_PLAY)
+                START_COLOR("color_good");
+        else if (state == MPD_STATE_PAUSE)
+                START_COLOR("color_degraded");
+
+        mpd_status_free(status);
+        song = mpd_run_current_song(conn);
+
+        COPY_CROP(titlebuf, mpd_song_get_tag(song, MPD_TAG_TITLE, 0));
+        COPY_CROP(artistbuf, mpd_song_get_tag(song, MPD_TAG_ARTIST, 0));
+        COPY_CROP(albumbuf, mpd_song_get_tag(song, MPD_TAG_ALBUM, 0));
+        COPY_CROP(trackbuf, mpd_song_get_tag(song, MPD_TAG_TRACK, 0));
+        COPY_CROP(datebuf, mpd_song_get_tag(song, MPD_TAG_DATE, 0));
+
+        mpd_song_free(song);
+
+        for (walk = format; *walk != '\0'; walk++) {
+                if (*walk != '%') {
+                        *(outwalk++) = *walk;
+                        continue;
+                }
+
+                if (BEGINS_WITH(walk+1, "title")) {
+                        if (*titlebuf)
+                                outwalk += sprintf(outwalk, "%s", titlebuf);
+                        walk += strlen("title");
+                }
+                else if (BEGINS_WITH(walk+1, "artist")) {
+                        if (*artistbuf)
+                                outwalk += sprintf(outwalk, "%s", artistbuf);
+                        walk += strlen("artist");
+                }
+                else if (BEGINS_WITH(walk+1, "album")) {
+                        if (*albumbuf)
+                                outwalk += sprintf(outwalk, "%s", albumbuf);
+                        walk += strlen("album");
+                }
+                else if (BEGINS_WITH(walk+1, "track")) {
+                        if (*trackbuf)
+                                outwalk += sprintf(outwalk, "%s", trackbuf);
+                        walk += strlen("track");
+                }
+                else if (BEGINS_WITH(walk+1, "date")) {
+                        if (*datebuf)
+                                outwalk += sprintf(outwalk, "%s", datebuf);
+                        walk += strlen("date");
+                }
         }
 
 print_end:
