4301e32ff378169f95eb0ca7821a8805b4180c80
lbonn <bonnans.l@gmail.com>
2015-08-19 23:56:19 +0200

    mpd: do not crop in the middle of utf8 chars

--- src/print_mpd.c
+++ src/print_mpd.c
@@ -9,17 +9,71 @@
 
 #include "i3status.h"
 
-#define COPY_CROP(dst, src)                            \
-    do {                                               \
-        const char *tsrc = src;                        \
-        const unsigned int maxcplen = sizeof(dst) - 4; \
-        if (src) {                                     \
-            strncpy(dst, tsrc, maxcplen + 1);          \
-            if (strlen(tsrc) > maxcplen) {             \
-                strcpy(dst + maxcplen, "...");         \
-            }                                          \
-        }                                              \
-    } while (0)
+static unsigned int utf8_count_bytes(char c) {
+    unsigned char uc = (unsigned char)c;
+
+    if ((uc & 0xfc) == 0xfc) {
+        return 6;
+    }
+    if ((uc & 0xf8) == 0xf8) {
+        return 5;
+    }
+    if ((uc & 0xf0) == 0xf0) {
+        return 4;
+    }
+    if ((uc & 0xe0) == 0xe0) {
+        return 3;
+    }
+    if ((uc & 0xc0) == 0xc0) {
+        return 2;
+    }
+
+    return 1;
+}
+
+static void _copy_crop(char *dst, const char *src, size_t dst_len) {
+    size_t crop_len = dst_len - 4;
+    size_t k;
+
+    if (src) {
+        size_t src_len = strlen(src);
+        size_t ck = 0;
+        unsigned int utf8_bytes_rem = 0;
+
+        /* copy and respect these conditions:
+         * - strlen(dst) < crop_len
+         * - unterminated utf8 chars are discarded
+         */
+        for (k = 0; k < crop_len; k++) {
+            if (src[k] == 0) {
+                break;
+            }
+
+            dst[k] = src[k];
+
+            if (utf8_bytes_rem == 0) {
+                /* expecting a new char */
+                utf8_bytes_rem = utf8_count_bytes(src[k]) - 1;
+            } else {
+                utf8_bytes_rem -= 1;
+            }
+
+            if (utf8_bytes_rem == 0) {
+                /* end of a char */
+                ck = k + 1;
+            }
+        }
+
+        if (src_len > ck) {
+            strcpy(dst + ck, "...");
+            ck += 3;
+        }
+
+        dst[ck] = 0;
+    }
+}
+
+#define copy_crop(dst, src) _copy_crop(dst, src, sizeof(dst))
 
 void print_mpd(yajl_gen json_gen, char *buffer, const char *host, int port, const char *password, const char *format) {
     const char *walk;
@@ -83,11 +137,11 @@ void print_mpd(yajl_gen json_gen, char *buffer, const char *host, int port, cons
 
     song = mpd_run_current_song(conn);
 
-    COPY_CROP(titlebuf, mpd_song_get_tag(song, MPD_TAG_TITLE, 0));
-    COPY_CROP(artistbuf, mpd_song_get_tag(song, MPD_TAG_ARTIST, 0));
-    COPY_CROP(albumbuf, mpd_song_get_tag(song, MPD_TAG_ALBUM, 0));
-    COPY_CROP(trackbuf, mpd_song_get_tag(song, MPD_TAG_TRACK, 0));
-    COPY_CROP(datebuf, mpd_song_get_tag(song, MPD_TAG_DATE, 0));
+    copy_crop(titlebuf, mpd_song_get_tag(song, MPD_TAG_TITLE, 0));
+    copy_crop(artistbuf, mpd_song_get_tag(song, MPD_TAG_ARTIST, 0));
+    copy_crop(albumbuf, mpd_song_get_tag(song, MPD_TAG_ALBUM, 0));
+    copy_crop(trackbuf, mpd_song_get_tag(song, MPD_TAG_TRACK, 0));
+    copy_crop(datebuf, mpd_song_get_tag(song, MPD_TAG_DATE, 0));
 
     mpd_song_free(song);
 
